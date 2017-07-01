fe6b243f39cfb29ac104922095aa5337d66aae87
HD1 Agent <drakmir+bbs@holodeckone.com>
2017-07-01 06:31:48 +0000

    Added hd1 copy shell command

---
+++ copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -f common/target/viaversion-common-*-SNAPSHOT.jar "$1"
+cp -f bukkit/target/viaversion-bukkit-*-SNAPSHOT.jar "$1"
+cp -f bungee/target/viaversion-bungee-*-SNAPSHOT.jar "$1"
